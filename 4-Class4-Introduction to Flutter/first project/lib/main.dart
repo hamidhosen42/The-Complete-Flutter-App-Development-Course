7d307cc8e7f01bfba3e1ead61cd856d5c7abc191
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-04-07 22:52:11 +0600

    Add files via upload

---
+++ 4-Class4-Introduction to Flutter/first project/lib/main.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        // ignore: prefer_const_literals_to_create_immutables
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+        ]),
+      ),
+    );
+  }
+}
